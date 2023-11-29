4eb45daf567f285e242ae2dcb874bc5051d68cce
子牧 <897136043@qq.com>
2023-11-29 15:22:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,8 @@ const LIMIT_AI_IMAGE_COUNT = 5;
 const LIMIT_COUNT_RESPONSE = '对不起，因为ChatGPT调用收费，您的免费使用额度已用完~'
 
 const configuration = new Configuration({
-  apiKey: '##your api key##',
+  apiKey: 'sk-jE9uK0XfKn0NDHr8JNdVlJ2p2tE2QVlIro1h1WmvuEjWWpGN',
+  apiEndpoint: 'https://api.aiproxy.io',
 });
 
 const openai = new OpenAIApi(configuration);
